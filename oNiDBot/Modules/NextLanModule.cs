61ab2fe2dda0a4574bf62ccd2596e8461e594de3
Stephen Riddell-Roberts <me@ste-roberts.co.uk>
2019-03-21 16:18:02 +0000

    Initial commit

---
+++ oNiDBot/Modules/NextLanModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace oNiDBot.Modules
+{
+    public class NextLanModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("NextLan")]
+        public Task NextLan()
+        {
+            string htmlContents = "";
+            using(var client = new WebClient())
+            {
+                htmlContents = client.DownloadString("https://lan.lsucs.org.uk/");
+            }
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(htmlContents);
+
+            var lanName = doc.DocumentNode.SelectSingleNode("//*[@id=\"lan\"]").InnerText;
+            var startDatestr = doc.DocumentNode.SelectSingleNode("//*[@id=\"title-container\"]/script").InnerText;
+            startDatestr = startDatestr.Replace("var countdown_start = \"", "");
+            startDatestr = startDatestr.Replace("\";", "");
+
+            var startDate = DateTime.Parse(startDatestr);
+
+            var days = Math.Round((  startDate - DateTime.Now ).TotalDays,0);
+
+            return ReplyAsync(
+                $"{lanName} starts {startDate.ToString("dddd, dd MMMM yyyy")} ({days} days)\n");
+
+        }
+            
+    }
+
+}
---
+++ oNiDBot/Program.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+using oNiDBot.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oNiDBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+
+            _client = new DiscordSocketClient();
+          
+
+            var services = ConfigureServices();
+            
+            await services.GetRequiredService<CommandHandlingService>().InitializeAsync(services);
+            
+            await _client.LoginAsync(TokenType.Bot, Context.Token);
+            await _client.StartAsync();
+            
+            await Task.Delay(-1);
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                // Base
+                .AddSingleton(_client)
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+              
+                // Add additional services here...
+                .BuildServiceProvider();
+        }
+
+        
+    }
+}
